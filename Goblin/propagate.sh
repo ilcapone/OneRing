9ae7a35eed59e1ea02e4df2bd42e321e238ff20b
ilcapone <momo@DESKTOP-PNDAFUE.localdomain>
2019-10-13 21:53:16 +0200

    add propagate

---
+++ Goblin/propagate.sh
@@ -0,0 +1,21 @@
+projects=$(gcloud projects list --format json | jq -r '.[] | .projectId')
+for project in $projects
+do
+	clusters_names=$(gcloud container clusters list --project $project --format json | jq -r '.[] | .name')
+	clusters_zones=$(gcloud container clusters list --project $project --format json | jq -r '.[] | .zone')
+	zone_l=""
+	i=0
+	for zone in $clusters_zones
+	do
+		zone_l[i]=$zone
+		i=$((i+1))
+	done
+	i=0
+	for cluster in $clusters_names
+	do
+		echo "-----------------------------------------"
+		gcloud container clusters get-credentials $cluster --zone ${zone_l[i]} --project $project
+		kubectl get pods
+		i=$((i+1))
+	done
+done
