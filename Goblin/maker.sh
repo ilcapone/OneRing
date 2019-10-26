a77b219de2f1990c9b69d7ac94cc87d8bb0383fd
ilcapone <momo@DESKTOP-PNDAFUE.localdomain>
2019-10-26 02:57:22 +0200

    chioo

--- Goblin/maker.sh
+++ Goblin/maker.sh
@@ -1,5 +1,5 @@
 #gcloud beta iam service-accounts create $1 --project $2
 #gcloud projects add-iam-policy-binding $2 --member serviceAccount:$1@$2.iam.gserviceaccount.com --role roles/container.developer
 #gcloud iam service-accounts keys create ~/key.json --iam-account $1@$2.iam.gserviceaccount.com
-gcloud organizations add-iam-policy-binding 80431372572--member=serviceAccount:$1 --role roles/browser
+gcloud organizations add-iam-policy-binding {OrgID} --member=serviceAccount:$1 --role roles/browser
 gcloud projects add-iam-policy-binding $2 --member serviceAccount:$2 --role roles/container.developer
