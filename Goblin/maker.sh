74da432bc5d93db2b5c3950a828f27586d3195cf
ilcapone <momo@DESKTOP-PNDAFUE.localdomain>
2019-10-26 18:14:34 +0200

    cleaning maker

--- Goblin/maker.sh
+++ Goblin/maker.sh
@@ -1,5 +1,19 @@
-#gcloud beta iam service-accounts create $1 --project $2
-#gcloud projects add-iam-policy-binding $2 --member serviceAccount:$1@$2.iam.gserviceaccount.com --role roles/container.developer
-#gcloud iam service-accounts keys create ~/key.json --iam-account $1@$2.iam.gserviceaccount.com
-gcloud organizations add-iam-policy-binding {OrgID} --member=serviceAccount:$1 --role roles/browser
-gcloud projects add-iam-policy-binding $2 --member serviceAccount:$2 --role roles/container.developer
+sa_name=$1
+project_id=$2
+org=$3
+echo $sa_name $project_id $org
+echo "Creating SA"
+gcloud beta iam service-accounts create $sa_name --project $project_id
+echo "Binding Browser Role or level"
+gcloud organizations add-iam-policy-binding $org --member=serviceAccount:$sa_name@$project_id.iam.gserviceaccount.com --role roles/browser
+echo "Extractig list projects"
+projects=$(gcloud projects list --format json | jq -r '.[] | .projectId')
+for project in $projects
+do
+	echo "Binding contaier role in $project"
+	gcloud projects add-iam-policy-binding $project --member serviceAccount:$sa_name@$project_id.iam.gserviceaccount.com --role roles/container.developer
+done
+echo "Download JSON Key"
+gcloud iam service-accounts keys create ~/key.json --iam-account $sa_name@$project_id.iam.gserviceaccount.com
+gcloud auth activate-service-account --key-file ~/key.json
+gcloud auth list
